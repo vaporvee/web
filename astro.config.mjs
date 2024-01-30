2f94f4854a32d21861ff1444af155a52178bc00c
vaporvee <yannik@vaporvee.com>
2024-01-30 23:16:05 +0100

    finished navbar and continued home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import minify from 'astro-min';
+
+import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [minify(), metaTags()]
+});
