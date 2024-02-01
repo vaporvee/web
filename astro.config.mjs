a77cbf4a499766fbe67ca8584858d3299a325548
vaporvee <yannik@vaporvee.com>
2024-02-01 14:50:19 +0100

    started coding the docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import minify from 'astro-min';
-
 import metaTags from "astro-meta-tags";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [minify(), metaTags()]
+  integrations: [minify(), metaTags(), mdx()]
 });
