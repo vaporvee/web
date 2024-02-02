73d849325fdcd69471287105f7fd0a1b5b6fbdec
vaporvee <yannik@vaporvee.com>
2024-02-02 23:03:55 +0100

    changed title formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,22 @@ import metaTags from "astro-meta-tags";
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import remarkDirective from 'remark-directive';
-import remarkCalloutDirectives from "@microflash/remark-callout-directives"
+import remarkCalloutDirectives from "@microflash/remark-callout-directives";
 import remarkExternalLinks from 'remark-external-links';
-
 import mdx from "@astrojs/mdx";
+
+import lighthouse from "astro-lighthouse";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [minify(), metaTags(), mdx()],
+  integrations: [minify(), metaTags(), mdx(), lighthouse()],
   markdown: {
     rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
       behavior: 'wrap'
     }]],
-    remarkPlugins: [remarkDirective, remarkCalloutDirectives, [remarkExternalLinks, { target: '_blank' }]],
+    remarkPlugins: [remarkDirective, remarkCalloutDirectives, [remarkExternalLinks, {
+      target: '_blank'
+    }]],
     drafts: true,
     shikiConfig: {
       theme: 'dracula'
