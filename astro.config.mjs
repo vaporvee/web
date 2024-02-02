2f962e3e900c6731bebc982458a398fd0121f804
vaporvee <yannik@vaporvee.com>
2024-02-02 10:02:31 +0100

    succesfully implemented mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 import { defineConfig } from 'astro/config';
 import minify from 'astro-min';
 import metaTags from "astro-meta-tags";
-import rehypeSlug from 'rehype-slug'
+import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [minify(), metaTags()],
+  integrations: [minify(), metaTags(), mdx()],
   markdown: {
-    rehypePlugins: [
-      rehypeSlug,
-      [rehypeAutolinkHeadings, { behavior: 'prepend' }],
-    ],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
+      behavior: 'prepend'
+    }]],
     drafts: true,
     shikiConfig: {
-      theme: 'dracula',
-    },
-  },
+      theme: 'dracula'
+    }
+  }
 });
