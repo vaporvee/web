17a07938435ce204c4d28a673841caed75858526
vaporvee <yannik@vaporvee.com>
2024-02-02 21:47:13 +0100

    finished markdown for docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import minify from 'astro-min';
 import metaTags from "astro-meta-tags";
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import remarkDirective from 'remark-directive';
+import remarkCalloutDirectives from "@microflash/remark-callout-directives"
+import remarkExternalLinks from 'remark-external-links';
 
 import mdx from "@astrojs/mdx";
 
@@ -11,8 +14,9 @@ export default defineConfig({
   integrations: [minify(), metaTags(), mdx()],
   markdown: {
     rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
-      behavior: 'prepend'
+      behavior: 'wrap'
     }]],
+    remarkPlugins: [remarkDirective, remarkCalloutDirectives, [remarkExternalLinks, { target: '_blank' }]],
     drafts: true,
     shikiConfig: {
       theme: 'dracula'
