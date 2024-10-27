c401f6284548e8229830ee846bb64425e3c52939
vaporvee <yannik@vaporvee.com>
2024-10-27 20:54:34 +0100

    maybe i should read the docs next time

--- next.config.ts
+++ next.config.ts
@@ -16,9 +16,9 @@ const nextConfig: NextConfig = {
   headers: async () => {
     return [
       {
-        source: "/**",
         headers: [
           {
+            source: "/:path*",
             key: "X-Frame-Options",
             value: "ALLOWALL"
           },
